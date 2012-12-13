ff0a8e0eb42cc66566074b077ccb99cf3ac16b21
Jason Castor <castor.jason@gmail.com>
2012-12-13 13:31:23 -0800

    added caching to the class view as it is quite large

--- urls.py
+++ urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, include, url
-from django.views.generic import DetailView, ListView
+from django.views.generic import DetailView, ListView, TemplateView
 from jpparse.models import Class, Method
-
+from jpparse.views import ClassListView
+from django.views.decorators.cache import cache_page
 urlpatterns = patterns('jpparse.views',
-	url(r'^$', 'mainview'),
-	url(r'^classes/$', ListView.as_view(model=Class,context_object_name="class_list",), name="class_list_view"),
+	url(r'^$',TemplateView.as_view(template_name="jpparse/info.html"), name="main"),
+	url(r'^info/$', TemplateView.as_view(template_name="jpparse/info.html"), name="info"),
+	url(r'^classes/$', cache_page(ClassListView.as_view(), 60*10), name="class_list_view"),
 	url(r'^classes/(?P<pk>\d+)/$', DetailView.as_view(model=Class,), name="class_detail_view"),
 	url(r'^method/(?P<pk>\d+)/$', DetailView.as_view(model=Method,), name="method_detail_view"),
 )
--- views.py
+++ views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-def mainview(request):
-	return render_to_response("jpparse/index.html", RequestContext(request))
+from django.views.generic import ListView, DetailView, TemplateView
+from jpparse.models import Class, Method
+from django.views.decorators.cache import cache_page
+
+class ClassListView(ListView):
+	model = Class
+	context_object_name = "class_list"
